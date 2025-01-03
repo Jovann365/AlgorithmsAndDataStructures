a101caa6eed589dcc24445ee412d5967644a7735
Jovan Velichkoski <jovanvelickoski@gmail.com>
2025-01-03 14:05:11 +0100

    pathsum2

---
+++ LeetCode/Trees/PathSumII.java
@@ -0,0 +1,44 @@
+package LeetCode.Trees;
+
+import DataStructures.BTree.TreeNode;
+import DataStructures.Tree.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PathSumII {
+    public static List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> currList = new ArrayList<>();
+        pathSumUtil(root, targetSum, result, currList, 0);
+        return result;
+    }
+
+    public static void pathSumUtil(TreeNode node, int targetSum, List<List<Integer>> result, List<Integer> currList, int sum) {
+       if (node == null)
+           return;
+       if (sum + node.val != targetSum || (sum + node.val == targetSum && node.left != null && node.right != null)){
+           currList.add(node.val);
+           sum += node.val;
+       }
+       else if (sum + node.val == targetSum && node.left == null && node.right == null) {
+           currList.add(node.val);
+           result.add(currList);
+       }
+       if (node.left != null) {
+           pathSumUtil(node.left, targetSum, result, currList, sum);
+       }
+       if (node.right != null) {
+           pathSumUtil(node.right, targetSum, result, currList, sum);
+       }
+       currList.remove(node.val);
+        sum -= node.val;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        System.out.println(pathSum(root, 3).toString());
+    }
+}
